e8d6149a9205d14db36be5bfc4e2e8ec6412cb76
ThearithBEN <89979065+ThearithBEN@users.noreply.github.com>
2023-06-26 20:14:19 +0700

    Add files via upload

---
+++ OOP 1/OOP 1/Products.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    public class Products
+    {
+        // Data member / Field / Properties
+        private string itemname;
+        private int qty;
+        private double price;
+
+        // Method / Event
+            // Constructor
+        public Products()
+        {
+            itemname = "Coke";
+            qty = 10;
+            price = 70;
+        }
+        public Products( string name, int q, double price)
+        {
+            itemname = name;
+            qty = q;
+            this.price = price;
+        }
+
+        //Setter
+        public void SetItemName( string name )
+        {
+            this.itemname = name;
+        }
+        public void SetQty(int q)
+        {
+            this.qty = q;
+        }
+        public void SetPrice(double price)
+        {
+            this.price = price;
+        }
+
+        // Getter
+        public string GetItemName()
+        {
+            return this.itemname;
+        }
+        public int GetQty()
+        {
+            return this.qty;
+        }
+        public double GetPrice()
+        {
+            return this.price;
+        }
+
+        // ToString()
+
+
+
+    }
+}
---
+++ OOP 1/OOP 1/testing.cs
@@ -0,0 +1,15 @@
+namespace OOP_1
+{
+    public partial class testing : Form
+    {
+        public testing()
+        {
+            InitializeComponent();
+        }
+
+        private void Test_1_Load(object sender, EventArgs e)
+        {
+            Products item = new Products();
+        }
+    }
+}
